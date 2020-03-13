c94e2c1232cfde20cbbc4006e84b23ff292e9e28
Nik <sidnev.nick@gmail.com>
2020-03-13 15:46:24 +0300

    preserve order for adding handlers (#23)

--- tests/test_bots.py
+++ tests/test_bots.py
@@ -24,16 +24,16 @@ async def test_returning_bot_commands_status(
     status = await bot.status()
     commands = [command.body for command in status.result.commands]
     assert commands == [
-        "/regular-handler-that-included-in-status-by-callable-function",
-        "/regular-handler-with-background-dependencies",
-        "/regular-handler-with-name",
-        "/handler-command1",
-        "/handler-command2",
-        "/handler-command4",
-        "/handler-command5",
-        "/handler-command3",
         "/regular-handler",
         "/handler-command",
+        "/handler-command1",
+        "/handler-command2",
+        "/handler-command3",
+        "/handler-command4",
+        "/handler-command5",
+        "/regular-handler-with-name",
+        "/regular-handler-with-background-dependencies",
+        "/regular-handler-that-included-in-status-by-callable-function",
     ]
 
 
