9ee4e9c8fa10f010034473f41251bb8a40d14df7
Nik Sidnev <sidnev.nick@gmail.com>
2020-06-02 17:57:13 +0300

    ignore __all__ in botx.testing

--- botx/testing/__init__.py
+++ botx/testing/__init__.py
@@ -3,4 +3,4 @@
 from botx.testing.building.builder import MessageBuilder
 from botx.testing.testing_client.client import TestClient
 
-__all__ = ("TestClient", "MessageBuilder")
+__all__ = ("TestClient", "MessageBuilder")  # noqa: WPS410
