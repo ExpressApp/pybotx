12bb5af4b049fe0358b9284ef1ff9c79ef0c0111
Nik Sidnev <sidnev.nick@gmail.com>
2020-06-02 20:54:22 +0300

    fix import of test client in botx.testing

--- botx/testing/__init__.py
+++ botx/testing/__init__.py
@@ -1,6 +1,10 @@
 """Definition of entities for using in tests."""
 
 from botx.testing.building.builder import MessageBuilder
-from botx.testing.testing_client.client import TestClient
+
+try:
+    from botx.testing.testing_client.client import TestClient  # noqa: WPS433
+except ImportError:
+    TestClient = None  # type: ignore  # noqa: WPS440
 
 __all__ = ("TestClient", "MessageBuilder")  # noqa: WPS410
