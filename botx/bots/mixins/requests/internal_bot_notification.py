f3d7eb07429f7d0572b86502c29d7e20504b59de
vkarabanov <vkarabanov@ccsteam.ru>
2021-08-16 03:07:21 +0300

    feat: internal bot notification

---
+++ botx/bots/mixins/requests/internal_bot_notification.py
@@ -0,0 +1,41 @@
+"""Mixin for shortcut for internal bot notification resource requests."""
+
+from typing import Any, Dict, List, Optional
+from uuid import UUID
+
+from botx.bots.mixins.requests.call_protocol import BotXMethodCallProtocol
+from botx.clients.methods.v4.bots.internal_bot_notification import (
+    InternalBotNotification,
+)
+from botx.clients.types.message_payload import InternalBotNotificationPayload
+from botx.models.messages.sending.credentials import SendingCredentials
+
+
+class InternalBotNotificationRequestsMixin:
+    """Mixin for shortcut for internal bot notification resource requests."""
+
+    async def internal_bot_notification(
+        self: BotXMethodCallProtocol,
+        credentials: SendingCredentials,
+        text: str,
+        group_chat_id: UUID,
+        recipients: Optional[List[UUID]] = None,
+        opts: Optional[Dict[str, Any]] = None,
+    ) -> UUID:
+        """Send internal bot notifications into chat.
+
+        Arguments:
+            credentials: credentials for making request.
+            payload: payload for notification.
+            group_chat_id: ID of chats into which message should be sent.
+        """
+
+        return await self.call_method(
+            InternalBotNotification(
+                group_chat_id=group_chat_id,
+                recipients=recipients,
+                data=InternalBotNotificationPayload(message=text),
+                opts=opts or {},
+            ),
+            credentials=credentials,
+        )
