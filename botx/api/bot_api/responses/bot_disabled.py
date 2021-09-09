9e71abc032df2061d64de1d677e9f20d8ad09d8f
Arseniy Zhiltsov <arseniy.zhiltsov@ccsteam.ru>
2021-09-09 16:01:13 +0300

    Add docs

--- botx/api/bot_api/responses/bot_disabled.py
+++ botx/api/bot_api/responses/bot_disabled.py
@@ -14,12 +14,31 @@ class BotAPIBotDisabledErrorData:
 
 @dataclass
 class BotAPIBotDisabledResponse:
+    """Disabled bot response model.
+
+    Only `.error_data.status_message` attribute will be displayed to
+    user. Other attributes will be visible only in BotX logs.
+    """
+
     error_data: BotAPIBotDisabledErrorData
     errors: List[str] = field(default_factory=list)
     reason: Literal["bot_disabled"] = "bot_disabled"
 
 
 def build_bot_disabled_response(status_message: str) -> Dict[str, Any]:
+    """Build `bot disabled` response to BotX API.
+
+    Arguments:
+        status_message: Status message.
+
+    Returns:
+        built bot_disabled response.
+
+    It should be send if the bot can't process the command.
+
+    If you would like to build complex response, see
+    ::: botx.api.bot_api.responses.bot_disabled.BotAPIBotDisabledResponse
+    """
     response = BotAPIBotDisabledResponse(
         error_data=BotAPIBotDisabledErrorData(status_message=status_message),
     )
