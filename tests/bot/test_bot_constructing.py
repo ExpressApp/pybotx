7b504ac6edc178b6316e7f44b3fc33a7dcd2ad76
Arseniy Zhiltsov <arseniy.zhiltsov@ccsteam.ru>
2021-10-05 11:20:45 +0300

    Add empty bot accounts and collectors warning

--- tests/bot/test_bot_constructing.py
+++ tests/bot/test_bot_constructing.py
@@ -1,12 +1,24 @@
 import pytest
 
-from botx import Bot
+from botx import Bot, BotAccount, HandlerCollector
 
 
-def test__bot__required_at_least_one_collector() -> None:
+def test__bot__empty_collectors_warning(
+    loguru_caplog: pytest.LogCaptureFixture,
+    bot_account: BotAccount,
+) -> None:
     # - Act -
-    with pytest.raises(ValueError) as exc:
-        Bot(collectors=[], bot_accounts=[])
+    Bot(collectors=[], bot_accounts=[bot_account])
 
     # - Assert -
-    assert "at least one `HandlerCollector`" in str(exc)
+    assert "Bot has no connected collectors" in loguru_caplog.text
+
+
+def test__bot__empty_bot_accounts_warning(
+    loguru_caplog: pytest.LogCaptureFixture,
+) -> None:
+    # - Act -
+    Bot(collectors=[HandlerCollector()], bot_accounts=[])
+
+    # - Assert -
+    assert "Bot has no bot accounts" in loguru_caplog.text
