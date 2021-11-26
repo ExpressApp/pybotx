e34363a2f57b8668f6290421704545e925af9a6c
Alexander Samoylenko <lxmnk.inbox@gmail.com>
2021-11-26 23:18:29 +0300

    feat: Add `Chat` attribute to `DeletedFromChatEvent`

--- tests/bot/api/commands/system_events/test_deleted_from_chat.py
+++ tests/bot/api/commands/system_events/test_deleted_from_chat.py
@@ -3,9 +3,13 @@ from uuid import UUID
 
 import pytest
 
-from botx import Bot, HandlerCollector, lifespan_wrapper
-from botx.bot.models.commands.system_events.deleted_from_chat import (
+from botx import (
+    Bot,
+    Chat,
+    ChatTypes,
     DeletedFromChatEvent,
+    HandlerCollector,
+    lifespan_wrapper,
 )
 
 
@@ -70,11 +74,15 @@ async def test__deleted_from_chat__succeed() -> None:
 
     # - Assert -
     assert deleted_from_chat == DeletedFromChatEvent(
+        bot_id=UUID("bc7f96e2-91a5-5de4-8bde-23765450cac8"),
+        raw_command=None,
         huids=[
             UUID("ab103983-6001-44e9-889e-d55feb295494"),
             UUID("dcfa5a7c-7cc4-4c89-b6c0-80325604f9f4"),
         ],
-        chat_id=UUID("dea55ee4-7a9f-5da0-8c73-079f400ee517"),
-        bot_id=UUID("bc7f96e2-91a5-5de4-8bde-23765450cac8"),
-        raw_command=None,
+        chat=Chat(
+            id=UUID("dea55ee4-7a9f-5da0-8c73-079f400ee517"),
+            type=ChatTypes.GROUP_CHAT,
+            host="cts.example.com",
+        ),
     )
