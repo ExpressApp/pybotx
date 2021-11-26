6bfc67dba6f4a62a9838b0ef86428b8236371a8f
Alexander Samoylenko <lxmnk.inbox@gmail.com>
2021-11-26 23:12:24 +0300

    feat: Add `Chat` attribute to `AddedToChatEvent`

--- botx/bot/api/commands/system_events/added_to_chat.py
+++ botx/bot/api/commands/system_events/added_to_chat.py
@@ -5,8 +5,10 @@ from pydantic import Field
 
 from botx.bot.api.commands.base import BotAPIBaseCommand, BotAPIChatEventSender
 from botx.bot.api.enums import BotAPICommandTypes
+from botx.bot.models.commands.chat import Chat
 from botx.bot.models.commands.system_events.added_to_chat import AddedToChatEvent
 from botx.shared_models.api_base import VerifiedPayloadBaseModel
+from botx.shared_models.chat_types import convert_chat_type_to_domain
 
 try:
     from typing import Literal
@@ -34,4 +36,9 @@ class BotAPIAddedToChat(BotAPIBaseCommand):
             bot_id=self.bot_id,
             raw_command=raw_command,
             huids=self.payload.data.added_members,
+            chat=Chat(
+                id=self.sender.group_chat_id,
+                type=convert_chat_type_to_domain(self.sender.chat_type),
+                host=self.sender.host,
+            ),
         )
