de79d80b8f0b93486012350f298990072b155d53
Alexander Samoylenko <lxmnk.inbox@gmail.com>
2021-11-26 23:18:29 +0300

    feat: Add `Chat` attribute to `DeletedFromChatEvent`

--- botx/bot/api/commands/system_events/deleted_from_chat.py
+++ botx/bot/api/commands/system_events/deleted_from_chat.py
@@ -5,10 +5,12 @@ from pydantic import Field
 
 from botx.bot.api.commands.base import BotAPIBaseCommand, BotAPIChatEventSender
 from botx.bot.api.enums import BotAPICommandTypes
+from botx.bot.models.commands.chat import Chat
 from botx.bot.models.commands.system_events.deleted_from_chat import (
     DeletedFromChatEvent,
 )
 from botx.shared_models.api_base import VerifiedPayloadBaseModel
+from botx.shared_models.chat_types import convert_chat_type_to_domain
 
 try:
     from typing import Literal
@@ -35,5 +37,9 @@ class BotAPIDeletedFromChat(BotAPIBaseCommand):
             bot_id=self.bot_id,
             raw_command=raw_command,
             huids=self.payload.data.deleted_members,
-            chat_id=self.sender.group_chat_id,
+            chat=Chat(
+                id=self.sender.group_chat_id,
+                type=convert_chat_type_to_domain(self.sender.chat_type),
+                host=self.sender.host,
+            ),
         )
--- botx/bot/models/commands/system_events/deleted_from_chat.py
+++ botx/bot/models/commands/system_events/deleted_from_chat.py
@@ -3,6 +3,7 @@ from typing import List
 from uuid import UUID
 
 from botx.bot.models.commands.base import BotCommandBase
+from botx.bot.models.commands.chat import Chat
 
 
 @dataclass
@@ -15,4 +16,4 @@ class DeletedFromChatEvent(BotCommandBase):
     """
 
     huids: List[UUID]
-    chat_id: UUID
+    chat: Chat
