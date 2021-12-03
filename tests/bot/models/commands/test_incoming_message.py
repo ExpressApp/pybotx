3f04b5b657c2deda6a384c85572f19bf1ff2a5bf
Denis <d.blinkovv@yandex.ru>
2021-12-03 10:35:53 +0300

    Add argument & arguments property for incoming message (#250)

--- tests/bot/models/commands/test_incoming_message.py
+++ tests/bot/models/commands/test_incoming_message.py
@@ -1,4 +1,6 @@
-from typing import Callable
+from typing import Callable, Tuple
+
+import pytest
 
 from botx import IncomingMessage
 
@@ -21,3 +23,79 @@ def test__upn_property__filled(
 
     # - Assert -
     assert message.sender.upn == "login@domain"
+
+
+@pytest.mark.parametrize(
+    "body,argument_answer",
+    [
+        ("", ""),
+        ("/command", ""),
+    ],
+)
+def test__argument__not_filled(
+    incoming_message_factory: Callable[..., IncomingMessage],
+    body: str,
+    argument_answer: str,
+) -> None:
+    # - Arrange -
+    message = incoming_message_factory(body=body)
+
+    # - Assert -
+    assert message.argument == argument_answer
+
+
+@pytest.mark.parametrize(
+    "body,argument_answer",
+    [
+        ("/command arg1 ", "arg1"),
+        ("/command arg1  arg2", "arg1  arg2"),
+    ],
+)
+def test__argument__filled(
+    incoming_message_factory: Callable[..., IncomingMessage],
+    body: str,
+    argument_answer: str,
+) -> None:
+    # - Arrange -
+    message = incoming_message_factory(body=body)
+
+    # - Assert -
+    assert message.argument == argument_answer
+
+
+@pytest.mark.parametrize(
+    "body,argument_answer",
+    [
+        ("", ()),
+        ("/command", ()),
+    ],
+)
+def test__arguments__not_filled(
+    incoming_message_factory: Callable[..., IncomingMessage],
+    body: str,
+    argument_answer: Tuple[str, ...],
+) -> None:
+    # - Arrange -
+    message = incoming_message_factory(body=body)
+
+    # - Assert -
+    assert message.arguments == argument_answer
+
+
+@pytest.mark.parametrize(
+    "body,argument_answer",
+    [
+        ("/command arg1 ", ("arg1",)),
+        ("/command arg1  arg2", ("arg1", "arg2")),
+    ],
+)
+def test__arguments__filled(
+    incoming_message_factory: Callable[..., IncomingMessage],
+    body: str,
+    argument_answer: Tuple[str, ...],
+) -> None:
+    # - Arrange -
+    message = incoming_message_factory(body=body)
+
+    # - Assert -
+    assert message.arguments == argument_answer
