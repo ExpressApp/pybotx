02ce7cf723c4f75dbbcd738e4952da4a836f4d44
Arseniy Zhiltsov <arseniy.zhiltsov@ccsteam.ru>
2021-12-15 17:35:09 +0300

    Add sticker download method

--- botx/models/stickers.py
+++ botx/models/stickers.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 from typing import List
 from uuid import UUID
 
+from botx.async_buffer import AsyncBufferWritable
+from botx.bot.contextvars import bot_var
+
 
 @dataclass
 class Sticker:
@@ -17,6 +20,19 @@ class Sticker:
     id: UUID
     emoji: str
     image_link: str
+
+    async def download(
+        self,
+        async_buffer: AsyncBufferWritable,
+    ) -> None:
+        bot = bot_var.get()
+
+        response = await bot._httpx_client.get(self.image_link)  # noqa: WPS437
+        response.raise_for_status()
+
+        await async_buffer.write(response.content)
+
+        await async_buffer.seek(0)
 
 
 @dataclass
