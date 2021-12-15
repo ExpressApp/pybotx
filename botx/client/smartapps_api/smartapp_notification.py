1385123abe58c8d3f77d5094fc14ab7076d854d2
Denis Blinkov <d.blinkovv@yandex.ru>
2021-12-15 11:07:27 +0300

    Add smartapp notification method

---
+++ botx/client/smartapps_api/smartapp_notification.py
@@ -0,0 +1,61 @@
+from typing import Any, Dict
+from uuid import UUID
+
+from botx.client.authorized_botx_method import AuthorizedBotXMethod
+from botx.constants import SMARTAPP_API_VERSION
+from botx.missing import Missing
+from botx.models.api_base import UnverifiedPayloadBaseModel, VerifiedPayloadBaseModel
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal  # type: ignore  # noqa: WPS440
+
+
+class BotXAPISmartappNotificationRequestPayload(UnverifiedPayloadBaseModel):
+    group_chat_id: UUID
+    smartapp_counter: int
+    opts: Missing[Dict[str, Any]]
+    smartapp_api_version: int
+
+    @classmethod
+    def from_domain(
+        cls,
+        chat_id: UUID,
+        smartapp_counter: int,
+        opts: Missing[Dict[str, Any]],
+    ) -> "BotXAPISmartappNotificationRequestPayload":
+        return cls(
+            group_chat_id=chat_id,
+            smartapp_counter=smartapp_counter,
+            opts=opts,
+            smartapp_api_version=SMARTAPP_API_VERSION,
+        )
+
+
+class BotXAPISmartappNotificationResponsePayload(VerifiedPayloadBaseModel):
+    status: Literal["ok"]
+
+
+class SmartappNotificationMethod(AuthorizedBotXMethod):
+    async def execute(
+        self,
+        payload: BotXAPISmartappNotificationRequestPayload,
+    ) -> None:
+        path = "/api/v3/botx/smartapps/notification"
+
+        # TODO: Remove opts
+        # UnverifiedPayloadBaseModel.jsonable_dict remove empty dicts
+        json = payload.jsonable_dict()
+        json["opts"] = json.get("opts", {})
+
+        response = await self._botx_method_call(
+            "POST",
+            self._build_url(path),
+            json=json,
+        )
+
+        self._verify_and_extract_api_model(
+            BotXAPISmartappNotificationResponsePayload,
+            response,
+        )
---
+++ tests/client/smartapps_api/test_smartapp_notification.py
@@ -0,0 +1,56 @@
+from http import HTTPStatus
+from uuid import UUID
+
+import httpx
+import pytest
+import respx
+
+from botx import Bot, BotAccount, HandlerCollector, lifespan_wrapper
+
+
+@respx.mock
+@pytest.mark.asyncio
+@pytest.mark.mock_authorization
+async def test__send_smartapp_notification__succeed(
+    httpx_client: httpx.AsyncClient,
+    host: str,
+    bot_id: UUID,
+    bot_account: BotAccount,
+) -> None:
+    # - Arrange -
+    endpoint = respx.post(
+        f"https://{host}/api/v3/botx/smartapps/notification",
+        headers={"Authorization": "Bearer token", "Content-Type": "application/json"},
+        json={
+            "group_chat_id": "705df263-6bfd-536a-9d51-13524afaab5c",
+            "smartapp_counter": 42,
+            "opts": {"message": "ping"},
+            "smartapp_api_version": 1,
+        },
+    ).mock(
+        return_value=httpx.Response(
+            HTTPStatus.ACCEPTED,
+            json={
+                "status": "ok",
+                "result": "smartapp_notification_pushed",
+            },
+        ),
+    )
+
+    built_bot = Bot(
+        collectors=[HandlerCollector()],
+        bot_accounts=[bot_account],
+        httpx_client=httpx_client,
+    )
+
+    # - Act -
+    async with lifespan_wrapper(built_bot) as bot:
+        await bot.send_smartapp_notification(
+            bot_id=bot_id,
+            chat_id=UUID("705df263-6bfd-536a-9d51-13524afaab5c"),
+            smartapp_counter=42,
+            opts={"message": "ping"},
+        )
+
+    # - Assert -
+    assert endpoint.called
