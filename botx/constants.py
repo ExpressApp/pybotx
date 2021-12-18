16665b484fb2e2c3a312a179082aaecea15256bb
Denis Blinkov <d.blinkovv@yandex.ru>
2021-12-18 18:07:24 +0300

    Fix typing on constants

--- botx/constants.py
+++ botx/constants.py
@@ -3,8 +3,8 @@ try:
 except ImportError:
     from typing_extensions import Final  # type: ignore  # noqa: WPS440
 
-CHUNK_SIZE: Final = 1024 * 1024  # 1Mb
-BOT_API_VERSION: Final = 4
-SMARTAPP_API_VERSION: Final = 1
-STICKER_IMAGE_MAX_SIZE: Final = 512 * 1024  # 512Kb
-STICKER_PACKS_PER_PAGE: Final = 10
+CHUNK_SIZE: Final[int] = 1024 * 1024  # 1Mb
+BOT_API_VERSION: Final[int] = 4
+SMARTAPP_API_VERSION: Final[int] = 1
+STICKER_IMAGE_MAX_SIZE: Final[int] = 512 * 1024  # 512Kb
+STICKER_PACKS_PER_PAGE: Final[int] = 10
