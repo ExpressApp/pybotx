3fad3f454e9b478d5a8b76dd0ebfaec5cc8414fd
Denis Blinkov <d.blinkovv@yandex.ru>
2022-01-20 01:11:08 +0300

    Add support `stop_typing` event

---
+++ botx/client/events_api/stop_typing_event.py
@@ -0,0 +1,34 @@
+from uuid import UUID
+
+from typing_extensions import Literal  # For python 3.7 support
+
+from botx.client.authorized_botx_method import AuthorizedBotXMethod
+from botx.models.api_base import UnverifiedPayloadBaseModel, VerifiedPayloadBaseModel
+
+
+class BotXAPIStopTypingEventRequestPayload(UnverifiedPayloadBaseModel):
+    group_chat_id: UUID
+
+    @classmethod
+    def from_domain(cls, chat_id: UUID) -> "BotXAPIStopTypingEventRequestPayload":
+        return cls(group_chat_id=chat_id)
+
+
+class BotXAPIStopTypingEventResponsePayload(VerifiedPayloadBaseModel):
+    status: Literal["ok"]
+
+
+class StopTypingEventMethod(AuthorizedBotXMethod):
+    async def execute(self, payload: BotXAPIStopTypingEventRequestPayload) -> None:
+        path = "/api/v3/botx/events/stop_typing"
+
+        response = await self._botx_method_call(
+            "POST",
+            self._build_url(path),
+            json=payload.jsonable_dict(),
+        )
+
+        self._verify_and_extract_api_model(
+            BotXAPIStopTypingEventResponsePayload,
+            response,
+        )
---
+++ tests/client/events_api/test_stop_typing_event.py
@@ -0,0 +1,48 @@
+from http import HTTPStatus
+from uuid import UUID
+
+import httpx
+import pytest
+import respx
+
+from botx import Bot, BotAccountWithSecret, HandlerCollector, lifespan_wrapper
+
+
+@respx.mock
+@pytest.mark.asyncio
+@pytest.mark.mock_authorization
+async def test__stop_typing__succeed(
+    httpx_client: httpx.AsyncClient,
+    host: str,
+    bot_id: UUID,
+    bot_account: BotAccountWithSecret,
+) -> None:
+    # - Arrange -
+    endpoint = respx.post(
+        f"https://{host}/api/v3/botx/events/stop_typing",
+        headers={"Authorization": "Bearer token", "Content-Type": "application/json"},
+        json={
+            "group_chat_id": "054af49e-5e18-4dca-ad73-4f96b6de63fa",
+        },
+    ).mock(
+        return_value=httpx.Response(
+            HTTPStatus.OK,
+            json={"status": "ok", "result": "stop_typing_event_pushed"},
+        ),
+    )
+
+    built_bot = Bot(
+        collectors=[HandlerCollector()],
+        bot_accounts=[bot_account],
+        httpx_client=httpx_client,
+    )
+
+    # - Act -
+    async with lifespan_wrapper(built_bot) as bot:
+        await bot.stop_typing(
+            bot_id=bot_id,
+            chat_id=UUID("054af49e-5e18-4dca-ad73-4f96b6de63fa"),
+        )
+
+    # - Assert -
+    assert endpoint.called
