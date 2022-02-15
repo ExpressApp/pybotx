f872eac632fa4145d611c2e1135ff31f30d03034
Renat Akzholov <31467120+rrrrs09@users.noreply.github.com>
2022-02-15 10:06:01 +0300

    feat: add `wait_botx_method_callback` method to bot

--- tests/client/test_botx_method_callback.py
+++ tests/client/test_botx_method_callback.py
@@ -27,6 +27,7 @@ from botx.client.botx_method import (
 )
 from botx.client.exceptions.base import BaseClientError
 from botx.missing import MissingOptional, Undefined, not_undefined
+from botx.models.method_callbacks import BotAPIMethodSuccessfulCallback
 from tests.client.test_botx_method import (
     BotXAPIFooBarRequestPayload,
     BotXAPIFooBarResponsePayload,
@@ -440,4 +441,60 @@ async def test__botx_method_callback__callback_successful_received(
 
     # - Assert -
     assert await task == UUID("21a9ec9e-f21f-4406-ac44-1a78d2ccf9e3")
+    assert endpoint.called
+
+
+async def test__botx_method_callback__bot_wait_callback(
+    respx_mock: MockRouter,
+    httpx_client: httpx.AsyncClient,
+    host: str,
+    bot_id: UUID,
+    bot_account: BotAccountWithSecret,
+) -> None:
+    # - Arrange -
+    endpoint = respx_mock.post(
+        f"https://{host}/foo/bar",
+        json={"baz": 1},
+        headers={"Content-Type": "application/json"},
+    ).mock(
+        return_value=httpx.Response(
+            HTTPStatus.ACCEPTED,
+            json={
+                "status": "ok",
+                "result": {"sync_id": "21a9ec9e-f21f-4406-ac44-1a78d2ccf9e3"},
+            },
+        ),
+    )
+    built_bot = Bot(
+        collectors=[HandlerCollector()],
+        bot_accounts=[bot_account],
+        httpx_client=httpx_client,
+    )
+
+    built_bot.call_foo_bar = types.MethodType(call_foo_bar, built_bot)
+
+    # - Act -
+    async with lifespan_wrapper(built_bot) as bot:
+        foo_bar = await bot.call_foo_bar(bot_id, baz=1, wait_callback=False)
+        task = asyncio.create_task(bot.wait_botx_method_callback(foo_bar, None))
+
+        # Return control to event loop
+        await asyncio.sleep(0)
+
+        bot.set_raw_botx_method_result(
+            {
+                "sync_id": "21a9ec9e-f21f-4406-ac44-1a78d2ccf9e3",
+                "status": "ok",
+                "result": {},
+            },
+        )
+
+        callback = await task
+
+    # - Assert -
+    assert callback == BotAPIMethodSuccessfulCallback(
+        sync_id=foo_bar,
+        status="ok",
+        result={},
+    )
     assert endpoint.called
