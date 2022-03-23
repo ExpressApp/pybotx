e152f3729d9e87652649c88a814e55ab1b42df66
Yegor <kutuzov-1313@yandex.ru>
2022-03-23 17:03:21 +0300

    feat: Add handling of unknown entity. (#338)

--- tests/test_incoming_message.py
+++ tests/test_incoming_message.py
@@ -473,3 +473,65 @@ async def test__async_execute_raw_bot_command__all_mention_types(
             ),
         ],
     )
+
+
+async def test__async_execute_raw_bot_command__unknown_entity_type(
+    bot_account: BotAccountWithSecret,
+    loguru_caplog: pytest.LogCaptureFixture,
+) -> None:
+    # - Arrange -
+    payload = {
+        "bot_id": "24348246-6791-4ac0-9d86-b948cd6a0e46",
+        "command": {
+            "body": "/hello",
+            "command_type": "user",
+            "data": {},
+            "metadata": {},
+        },
+        "attachments": [],
+        "async_files": [],
+        "entities": [
+            {
+                "type": "foo",
+                "data": {
+                    "foo": "bar",
+                },
+            },
+        ],
+        "source_sync_id": None,
+        "sync_id": "6f40a492-4b5f-54f3-87ee-77126d825b51",
+        "from": {
+            "ad_domain": None,
+            "ad_login": None,
+            "app_version": None,
+            "chat_type": "chat",
+            "device": None,
+            "device_meta": {
+                "permissions": None,
+                "pushes": False,
+                "timezone": "Europe/Moscow",
+            },
+            "device_software": None,
+            "group_chat_id": "30dc1980-643a-00ad-37fc-7cc10d74e935",
+            "host": "cts.example.com",
+            "is_admin": True,
+            "is_creator": True,
+            "locale": "en",
+            "manufacturer": None,
+            "platform": None,
+            "platform_package_id": None,
+            "user_huid": "f16cdc5f-6366-5552-9ecd-c36290ab3d11",
+            "username": None,
+        },
+        "proto_version": 4,
+    }
+
+    collector = HandlerCollector()
+    built_bot = Bot(collectors=[collector], bot_accounts=[bot_account])
+
+    # - Act -
+    async with lifespan_wrapper(built_bot) as bot:
+        bot.async_execute_raw_bot_command(payload)
+
+    # - Assert -
+    assert "Received unknown entity type" in loguru_caplog.text
