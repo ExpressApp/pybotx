c5c29c3b3f5eefc7dc8dae0858b7ed9a7544d5e8
Лисов Кирилл <liss-2001g@mail.ru>
2022-05-16 10:30:21 +0300

    Add fixture respx_mocking (#357)
    * Add fixture respx_mocking
    
    * Rename respx_mocking to prevent_http_requests

--- tests/conftest.py
+++ tests/conftest.py
@@ -251,3 +251,8 @@ def correct_handler_trigger() -> Mock:
 @pytest.fixture
 def incorrect_handler_trigger() -> Mock:
     return Mock()
+
+
+@pytest.fixture(autouse=True)
+def prevent_http_requests(respx_mock: MockRouter) -> None:
+    pass
