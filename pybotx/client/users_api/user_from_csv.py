268f42d82ff3b96ccec83e62db2816c149870909
Nikita Demidovich <nikita.demidovich@ccsteam.ru>
2022-10-27 14:41:29 +0300

    feat: Method for getting a list of users on a CTS

---
+++ pybotx/client/users_api/user_from_csv.py
@@ -0,0 +1,50 @@
+from typing import Optional
+from uuid import UUID
+
+from pydantic import Field, validator
+
+from pybotx.models.api_base import VerifiedPayloadBaseModel
+from pybotx.models.enums import (
+    APISyncSourceTypes,
+    APIUserKinds,
+    convert_sync_source_type_to_domain,
+    convert_user_kind_to_domain,
+)
+from pybotx.models.users import UserFromCSV
+
+
+class BotXAPIUserFromCSVResult(VerifiedPayloadBaseModel):
+    huid: UUID = Field(alias="HUID")
+    ad_login: str = Field(alias="AD Login")
+    ad_domain: str = Field(alias="Domain")
+    email: Optional[str] = Field(alias="AD E-mail")
+    name: str = Field(alias="Name")
+    sync_source: APISyncSourceTypes = Field(alias="Sync source")
+    active: bool = Field(alias="Active")
+    user_kind: APIUserKinds = Field(alias="Kind")
+    company: Optional[str] = Field(alias="Company")
+    department: Optional[str] = Field(alias="Department")
+    position: Optional[str] = Field(alias="Position")
+
+    @validator("email", "company", "department", "position", pre=True)
+    @classmethod
+    def replace_empty_string_with_none(cls, field_value: str) -> Optional[str]:
+        if field_value == "":
+            return None
+
+        return field_value
+
+    def to_domain(self) -> UserFromCSV:
+        return UserFromCSV(
+            huid=self.huid,
+            ad_login=self.ad_login,
+            ad_domain=self.ad_domain,
+            email=self.email,
+            username=self.name,
+            sync_source=convert_sync_source_type_to_domain(self.sync_source),
+            active=self.active,
+            user_kind=convert_user_kind_to_domain(self.user_kind),
+            company=self.company,
+            department=self.department,
+            position=self.position,
+        )
---
+++ pybotx/client/users_api/users_as_csv.py
@@ -0,0 +1,53 @@
+from aiocsv.readers import AsyncDictReader
+
+from pybotx.async_buffer import AsyncBufferUnicodeWritable
+from pybotx.client.authorized_botx_method import AuthorizedBotXMethod
+from pybotx.client.botx_method import response_exception_thrower
+from pybotx.client.exceptions.users import NoUserKindSelectedError
+from pybotx.models.api_base import UnverifiedPayloadBaseModel
+
+
+class BotXAPIUsersAsCSVRequestPayload(UnverifiedPayloadBaseModel):
+    cts_user: bool
+    unregistered: bool
+    botx: bool
+
+    @classmethod
+    def from_domain(
+        cls,
+        cts_user: bool,
+        unregistered: bool,
+        botx: bool,
+    ) -> "BotXAPIUsersAsCSVRequestPayload":
+        return cls(
+            cts_user=cts_user,
+            unregistered=unregistered,
+            botx=botx,
+        )
+
+
+class UsersAsCSVMethod(AuthorizedBotXMethod):
+    status_handlers = {
+        **AuthorizedBotXMethod.status_handlers,
+        400: response_exception_thrower(NoUserKindSelectedError),
+    }
+
+    async def execute(
+        self,
+        payload: BotXAPIUsersAsCSVRequestPayload,
+        async_buffer: AsyncBufferUnicodeWritable,
+    ) -> AsyncDictReader:
+        path = "/api/v3/botx/users/users_as_csv"
+
+        async with self._botx_method_stream(
+            "GET",
+            self._build_url(path),
+            params=payload.jsonable_dict(),
+        ) as response:
+            # https://github.com/nedbat/coveragepy/issues/1223
+            async for chunk in response.aiter_bytes():  # pragma: no cover
+                await async_buffer.write(chunk.decode())
+
+        await async_buffer.seek(0)
+
+        return AsyncDictReader(async_buffer)  # type: ignore[arg-type]
