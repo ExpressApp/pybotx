b928947a706713d71684792041912d4da35a7ed5
Alexander Maximenyuk <alex.maximenyuk@ccsteam.ru>
2023-06-21 16:59:20 +0300

    Made _UndefinedType as a Singleton class.

--- pybotx/missing.py
+++ pybotx/missing.py
@@ -1,8 +1,15 @@
-from typing import Literal, TypeVar, Union
+from typing import Any, List, Literal, TypeVar, Union
 
 
 class _UndefinedType:
     """For fields that can be skipped."""
+
+    _instances: List["_UndefinedType"] = []
+
+    def __new__(cls, *args: Any) -> "_UndefinedType":
+        if not cls._instances:
+            cls._instances.append(super().__new__(cls, *args))
+        return cls._instances[-1]
 
     def __bool__(self) -> Literal[False]:
         return False
