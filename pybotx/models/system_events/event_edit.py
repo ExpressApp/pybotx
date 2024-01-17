c3e556e085a2993b86056efe60b9f5d07d5103fe
denis.postnykh <denis.postnykh@cssteam.ru>
2024-01-17 10:21:02 +0300

    fix: Add attachments and entities in system event edit

--- pybotx/models/system_events/event_edit.py
+++ pybotx/models/system_events/event_edit.py
@@ -1,9 +1,14 @@
 from dataclasses import dataclass
-from typing import Any, Dict, Literal, Optional
+from typing import Any, Dict, List, Literal, Optional
 
 from pydantic import Field
 
 from pybotx.models.api_base import VerifiedPayloadBaseModel
+from pybotx.models.attachments import (
+    BotAPIAttachment,
+    IncomingAttachment,
+    convert_api_attachment_to_domain,
+)
 from pybotx.models.base_command import (
     BaseBotAPIContext,
     BotAPIBaseCommand,
@@ -12,6 +17,11 @@ from pybotx.models.base_command import (
 )
 from pybotx.models.bot_account import BotAccount
 from pybotx.models.enums import BotAPISystemEventTypes
+from pybotx.models.message.incoming_message import (
+    BotAPIEntity,
+    Entity,
+    convert_bot_api_entity_to_domain,
+)
 
 
 @dataclass
@@ -19,10 +29,14 @@ class EventEdit(BotCommandBase):
     """Event `system:event_edit`.
 
     Attributes:
-        body: updated message body.
+        body: Updated message body.
+        attachments: Attachments from updated message.
+        entities: Entities from updated message.
     """
 
     body: Optional[str]
+    attachments: List[IncomingAttachment]
+    entities: List[Entity]
 
 
 class BotAPIEventEditData(VerifiedPayloadBaseModel):
@@ -37,6 +51,8 @@ class BotAPIEventEditPayload(BotAPIBaseSystemEventPayload):
 class BotAPIEventEdit(BotAPIBaseCommand):
     payload: BotAPIEventEditPayload = Field(..., alias="command")
     sender: BaseBotAPIContext = Field(..., alias="from")
+    attachments: List[BotAPIAttachment]
+    entities: List[BotAPIEntity]
 
     def to_domain(self, raw_command: Dict[str, Any]) -> EventEdit:
         return EventEdit(
@@ -46,4 +62,15 @@ class BotAPIEventEdit(BotAPIBaseCommand):
             ),
             raw_command=raw_command,
             body=self.payload.data.body,
+            attachments=[
+                convert_api_attachment_to_domain(
+                    api_attachment=attachment,
+                    message_body=self.payload.body,
+                )
+                for attachment in self.attachments
+            ],
+            entities=[
+                convert_bot_api_entity_to_domain(api_entity=entity)
+                for entity in self.entities
+            ],
         )
